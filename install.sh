dbd28354a7270c78d6f4500d3c61c75722f661cf
RWejlgaard <wejlgaard@live.dk>
2015-09-19 00:01:32 +0200

    Added nethackrc

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cp bashrc ~/.bashrc
 cp vimrc ~/.vimrc
-cp fan ~/.fan
-exec spotifyinstall.sh
-exec postspotifyinstall.sh
+cp nethackrc ~/.nethackrc
+#exec spotifyinstall.sh
+#exec postspotifyinstall.sh
 echo "All Done!"
