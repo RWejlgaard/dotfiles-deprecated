58fae2dbfc61505b53fc519a103e6deb67509ac9
root <root@localhost.localdomain>
2016-10-06 07:43:29 +0000

    added vundle install script

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ cp vimrc ~/.vimrc
 cp nethackrc ~/.nethackrc
 cp -a vim ~/.vim
 cp tmux.conf ~/.tmux.conf 
+sh vundleinstall.sh
 #exec spotifyinstall.sh
 #exec postspotifyinstall.sh
 echo "All Done!"
